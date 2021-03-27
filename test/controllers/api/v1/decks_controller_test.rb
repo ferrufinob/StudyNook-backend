a580ab82db940ade017e3ea623bb8d735b918b28
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-27 12:15:16 -0400

    pluralized deck controller

--- test/controllers/api/v1/decks_controller_test.rb
+++ test/controllers/api/v1/decks_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Api::V1::DeckControllerTest < ActionDispatch::IntegrationTest
+class Api::V1::DecksControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
