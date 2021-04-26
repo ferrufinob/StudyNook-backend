cc797d5a314f582582e37a553a2bc146206a5b30
Brenda Ferrufino <ferrufinob@gmail.com>
2021-04-25 22:36:47 -0400

    clean up

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,7 +1,3 @@
 class Deck < ApplicationRecord
   has_many :cards
-
-  def card_count
-    self.cards.count
-  end
 end
--- app/serializers/deck_serializer.rb
+++ app/serializers/deck_serializer.rb
@@ -1,4 +1,4 @@
 class DeckSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :card_count
+  attributes :name
 end
