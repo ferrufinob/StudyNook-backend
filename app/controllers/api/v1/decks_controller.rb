3b4eec1d390d288ec673ea38cc584735a26f5811
Brenda Ferrufino <ferrufinob@gmail.com>
2021-09-11 15:33:06 -0400

    deck create action

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::DecksController < ApplicationController
     decks = Deck.all
     render json: DeckSerializer.new(decks)
   end
+
+  def create
+    deck = Deck.new(card_params)
+    if deck.save
+      render json: Deck.Serializer.new(card)
+    else
+      render json: { errors: deck.errors.full_messages.to_sentence }
+    end
+  end
 end
