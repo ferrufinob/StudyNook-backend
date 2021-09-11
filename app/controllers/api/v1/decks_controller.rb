6f60a92759ab143afe7ff33d0c000851138c294e
Brenda Ferrufino <ferrufinob@gmail.com>
2021-09-11 15:39:02 -0400

    added deck params func

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -5,11 +5,17 @@ class Api::V1::DecksController < ApplicationController
   end
 
   def create
-    deck = Deck.new(card_params)
+    deck = Deck.new(deck_params)
     if deck.save
       render json: Deck.Serializer.new(card)
     else
       render json: { errors: deck.errors.full_messages.to_sentence }
     end
+  end
+
+  private
+
+  def deck_params
+    params.require(:deck).permit(:name)
   end
 end
