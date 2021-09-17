509c143137c284d75f3dfa561951fc72bec41c10
Brenda Ferrufino <ferrufinob@gmail.com>
2021-09-17 01:20:30 -0400

    changed db to postgresl

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_27_040450) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cards", force: :cascade do |t|
     t.string "front"
     t.string "back"
